0dc9ef88cae7f689fa38086893316dfcb4569d12
Yoko Harada <yokolet@gmail.com>
2025-08-28 15:56:56 +0900

    adds more methods to Comment

--- src/main/java/zenzai/nodes/Comment.java
+++ src/main/java/zenzai/nodes/Comment.java
@@ -1,12 +1,14 @@
 package zenzai.nodes;
 
 import org.w3c.dom.DOMException;
+import zenzai.internal.QuietAppendable;
 
 public abstract class Comment extends LeafNode implements org.w3c.dom.Comment {
 
     /**
-     Create a new comment node.
-     @param data The contents of the comment
+     * Create a new comment node.
+     *
+     * @param data The contents of the comment
      */
     public Comment(String data) {
         super(data);
@@ -24,18 +26,51 @@ public abstract class Comment extends LeafNode implements org.w3c.dom.Comment {
         return getNodeValue();
     }
 
-    // org.w3c.dom.Node
-    @Override public String nodeName() {
+    // org.w3c.dom.CharacterData
+    // nodes.Comment
+    @Override
+    public String getData() throws DOMException {
+        return coreValue();
+    }
+
+    public abstract void setData(String data) throws DOMException;
+
+    public abstract int getLength();
+
+    public abstract String substringData(int offset, int length) throws DOMException;
+
+    public abstract void appendData(String data) throws DOMException;
+
+    public abstract void insertData(int offset, String data) throws DOMException;
+
+    public abstract void deleteData(int offset, int count) throws DOMException;
+
+    public abstract void replaceData(int offset, int count, String arg) throws DOMException;
+
+    @Override
+    public Comment clone() {
+        return (Comment) super.clone();
+    }
+
+    @Override
+    public String nodeName() {
         return "#comment";
     }
 
-    // org.w3c.dom.CharacterData
-    public abstract String getData() throws DOMException;
-    public abstract void setData(String data) throws DOMException;
-    public abstract int getLength();
-    public abstract String substringData(int offset, int length) throws DOMException;
-    public abstract void appendData(String data) throws DOMException;
-    public abstract void insertData(int offset, String data) throws DOMException;
-    public abstract void deleteData(int offset, int count) throws DOMException;
-    public abstract void replaceData(int offset, int count, String arg) throws DOMException;
+    public Comment setDataWithReturn(String data) {
+        coreValue(data);
+        return this;
+    }
+
+    @Override
+    void outerHtmlHead(QuietAppendable accum, Document.OutputSettings out) {
+        accum
+                .append("<!--")
+                .append(getData())
+                .append("-->");
+    }
+
+    private static boolean isXmlDeclarationData(String data) {
+        return (data.length() > 1 && (data.startsWith("!") || data.startsWith("?")));
+    }
 }
